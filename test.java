5486f488b5db79136c7fe8cb48efb8ea603353ef
Your Name <you@example.com>
2020-02-04 08:34:21 +0800

    first commit

---
+++ test.java
@@ -0,0 +1,13 @@
+class Test{
+	public static void main(String[]args){
+	}
+		System.out.println("Hello 1ISA");
+
+	public void sayHello(){
+		System.out.println("Hello");
+	}
+	
+	pulic int add(int a, int b){
+		return a+b;
+	}
+}
